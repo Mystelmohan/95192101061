77e874ba9a06075f756585e0310b1a22f140e213
Mystelmohan <147577443+Mystelmohan@users.noreply.github.com>
2024-05-02 13:22:52 +0530

    add

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -1,17 +1,12 @@
 import './App.css';
-import { Navbar } from './Componants/Navbar';
-import { Route, Routes } from 'react-router-dom';
+
 import { Home } from './Componants/Home';
-import { Products } from './Componants/Products';
+
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/products' element={<Products/>}/>
-      </Routes>
+     <Home/>
     </div>
   );
 }
--- e-commerce/src/Componants/Home.js
+++ e-commerce/src/Componants/Home.js
@@ -1,25 +1,29 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 export const Home = () => {
-  const[post,setpost]=useState([])
-  useEffect(()=>{axios.get("http://20.244.56.144/test/companies/AMZ/categories/Laptop/products?top=10&minPrice=1&maxPrice=10000")
-  .then(res=>setpost(res.data))
-  .catch(err=>console.log(err))
-  console.log(post)},[])
+  const[posts,setPosts]=useState([])
+  useEffect(()=>{axios.get("http://localhost:3001/Products").then(res=>setPosts(res.data)).catch(err=>console.log(err))
+  console.log(posts)},[])
 
   return (
     <div>
+        <h1>Products</h1>
+    <div className='container'>
+        
         {
-            post.map(x=>{
-                <div>
-                <p>{x.productName}</p>
-                <p>{x.price}</p>
-                <p>{x.rating}</p>
-                <p>{x.discount}</p>
-                <p>{x.availability}</p>
+            posts.map(x=>
+                
+                <div className='products'>
+                <p>Product Name: {x.productName}</p>
+                <p>Price : {x.price}</p>
+                <p>Rating : {x.rating}</p>
+                <p>Discount : {x.discount}</p>
+                <p>availability: {x.availability}</p>
                 </div>
-            })
+                
+            )
         }
+    </div>
     </div>
   )
 }
